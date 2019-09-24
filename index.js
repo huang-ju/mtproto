e9352036f5729d4745e3faf5424cc394039b0dcd
huangju <huangjugood@gmail.com>
2019-09-25 07:56:23 +0800

    修改代理

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = new Koa();
 
 const main = ctx => {
   ctx.response.type = 'json';
-  ctx.response.body = { server: 'iplc-hk1.crhnode.top', port: 540, secret: 'ddf5c322081360e73b40db06ea6539f7f2' }
+  ctx.response.body = { server: '45.92.124.97', port: 5003, secret: '7ia26nLk5DC6eKUzIOF1JpBzMy5hbWF6b25hd3MuY29t' }
 };
 
 app.use(main);
